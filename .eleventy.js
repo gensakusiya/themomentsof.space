18b58d3e25e304bdf0dd391c1e4b71e846a05595
Artyom Prikhodko <homoogenecus@gmail.com>
2020-05-10 14:51:00 +0300

    remove script transfer source files

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ module.exports = function (config) {
   config.addPassthroughCopy('src/fonts');
   config.addPassthroughCopy('src/images');
   config.addPassthroughCopy('src/styles');
-  config.addPassthroughCopy('src/scripts');
   config.addPassthroughCopy('src/manifest.json');
   
   // Config
